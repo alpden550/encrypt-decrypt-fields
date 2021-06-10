e2aa9e8f2480ff208566498054f6b101c43ff99b
Denis Novikov <alpden550@gmail.com>
2021-06-10 10:45:37 +0300

    feat(encrypt): add Crypt class

---
+++ django-encrypt_decrypt/crypt.py
@@ -0,0 +1,23 @@
+import base64
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+
+class Crypt:
+    def __init__(self, key: str):
+        self.key = key
+
+    def get_key(self) -> bytes:
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())
+        digest.update(self.key.encode())
+        return base64.urlsafe_b64encode(digest.finalize())
+
+    def encrypt_password(self, password: str) -> str:
+        fernet = Fernet(self.get_key())
+        return fernet.encrypt(password.encode()).decode()
+
+    def decrypt_token(self, token: str) -> str:
+        fernet = Fernet(self.get_key())
+        return fernet.decrypt(token.encode()).decode()
