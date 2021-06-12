103498461c90512cf1e80940ff1011f3884ecde0
Denis Novikov <alpden550@gmail.com>
2021-06-12 12:10:39 +0300

    feat(tests): add crypto tests

---
+++ tests/test_crypto.py
@@ -0,0 +1,40 @@
+import pytest
+
+from django_encrypt_decrypt import Crypto
+
+
+class TestCrypt:
+    def test_key_is_str(self):
+        crypto = Crypto("secret")
+        key = crypto.get_key()
+
+        assert isinstance(key, bytes)
+        assert key == b'K7gNU3sdo-OL0wNhqoVWhr3g6s1xYv72ol_pe_Unols='
+
+    def test_key_not_str(self):
+        crypto = Crypto(1)
+
+        with pytest.raises(AttributeError):
+            crypto.get_key()
+
+    def test_encrypt_successfully(self):
+        password = Crypto('secret').encrypt('password')
+
+        assert isinstance(password, str)
+        assert password != 'password'
+
+    @pytest.mark.parametrize("test_input", [int, tuple, list, dict])
+    def test_encrypt_not_successfully(self, test_input):
+        with pytest.raises(AttributeError):
+            Crypto('secret').encrypt(test_input)
+
+    def test_decrypt_empty_value(self):
+        password = Crypto('secret').decrypt_token('')
+
+        assert not password
+
+    def test_decrypt_successfully(self):
+        encrypted = Crypto('secret').encrypt('password')
+        decrypted = Crypto('secret').decrypt_token(encrypted)
+
+        assert decrypted == 'password'
