5a540e440508a8782049a55a8c73afe0cfdeb511
Denis Novikov <alpden550@gmail.com>
2021-06-25 20:30:22 +0300

    feat(alchemy): add sqlalchemy field

---
+++ encrypt_decrypt_field/alchemy_fields.py
@@ -0,0 +1,18 @@
+from sqlalchemy.types import TypeDecorator, Text
+
+from encrypt_decrypt_field import Crypto
+
+
+class EncryptedAlchemyTextField(TypeDecorator):
+    impl = Text
+
+    def __init__(self, key: str, *arg, **kwargs):
+        TypeDecorator.__init__(self, *arg, **kwargs)
+        self.key = key
+        self.crypto = Crypto(key=self.key)
+
+    def process_bind_param(self, value, dialect):
+        if not value or isinstance(value, bytes) or isinstance(value, memoryview):
+            return value
+
+        return self.crypto.encrypt(value)
