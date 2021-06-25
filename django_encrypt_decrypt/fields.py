2b37523a73c461b667d511e258fe43e4467b937d
Denis Novikov <alpden550@gmail.com>
2021-06-25 15:28:15 +0300

    fix(fields): use bytes instead string and not update if model was save

--- django_encrypt_decrypt/fields.py
+++ django_encrypt_decrypt/fields.py
@@ -4,8 +4,7 @@ from django.db import models
 from .crypto import Crypto
 
 
-class EncryptedTextField(models.TextField):
-    internal_type = "TextField"
+class EncryptedTextField(models.BinaryField):
 
     def __init__(self, *args, **kwargs):
         self.key = kwargs.pop('key', None)
@@ -15,13 +14,10 @@ class EncryptedTextField(models.TextField):
         self.crypto = Crypto(self.key)
         super().__init__(*args, **kwargs)
 
-    def get_internal_type(self):
-        return self.internal_type
-
     def get_db_prep_value(self, value, connection, prepared=False):
-        value = super().get_db_prep_value(value, connection, prepared)
-        if not value:
-            return ""
+        if not value or isinstance(value, bytes):
+            return value
 
         encrypted_text = self.crypto.encrypt(value)
-        return encrypted_text
+        encrypted_value = super().get_db_prep_value(encrypted_text, connection, prepared)
+        return encrypted_value
