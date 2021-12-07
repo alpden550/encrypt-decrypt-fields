0695ed106afa9ac9815ee69facb059aa5e7cefb7
Denis Novikov <alpden550@gmail.com>
2021-12-07 15:09:43 +0300

    chore(init): update version

--- encrypt_decrypt_fields/__init__.py
+++ encrypt_decrypt_fields/__init__.py
@@ -2,4 +2,4 @@ from .crypto import Crypto   # noqa
 from .alchemy_fields import EncryptedAlchemyBinaryField  # noqa
 from .django_fields import EncryptedBinaryField  # noqa
 
-__version__ = '1.1.2'
+__version__ = '1.1.3'
