b7fa2c00e6791eb989266539d138a066164e7106
Denis Novikov <alpden550@gmail.com>
2023-10-31 11:21:32 +0300

    refact(init): update imports

--- encrypt_decrypt_fields/__init__.py
+++ encrypt_decrypt_fields/__init__.py
@@ -1,5 +1,4 @@
-from .crypto import Crypto   # noqa
-from .alchemy_fields import EncryptedAlchemyBinaryField  # noqa
-from .django_fields import EncryptedBinaryField  # noqa
+from encrypt_decrypt_fields.services import Crypto  # noqa: F401
+from encrypt_decrypt_fields.orm import EncryptedAlchemyBinaryField, EncryptedBinaryField  # noqa: F401
 
-__version__ = '1.1.3'
+__version__ = "1.3.6"
